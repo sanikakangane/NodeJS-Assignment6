c061f13aa7fab873b2abd853db5840dab8f81681
Sanika Kangane <sanikakangane166@gmail.com>
2026-09-17 14:19:43 +0530

    Add files via upload

---
+++ router/apiRouter.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const router = express.Router()
+
+function routerLogger(req, res, next) {
+    const date = new Date().toLocaleString()
+    console.log(req.method, req.originalUrl, date)
+    next()
+}
+
+router.use(routerLogger)
+
+router.get("/students", (req, res) => {
+    res.send("Students List")
+})
+
+router.get("/courses", (req, res) => {
+    res.send("Courses List")
+})
+
+router.get("/faculty", (req, res) => {
+    res.send("Faculty List")
+})
+
+module.exports = router
